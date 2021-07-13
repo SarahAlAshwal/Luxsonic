5f437b31380159764d9e6d95a225271449093127
Sarah <sa.alashwal@gmail.com>
2021-07-12 22:29:37 -0400

    add loginForm style

--- client/src/components/loginForm/LoginForm.jsx
+++ client/src/components/loginForm/LoginForm.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+import './LoginForm.css';
+
 function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsername = (event) => setUsername(event.target.value);
+  const handlePassword = (event) => setPassword(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(username,password)
+  }
   return (
-    <form>
-      <input></input>
-      <input></input>
-      <button>Sign In</button>
+    <form id='login-form' onSubmit={handleSubmit}>
+      <input placeholder='Username' type='text' className='login-input' onChange={handleUsername}></input>
+      <input placeholder='Password' type='password' className='login-input' onChange={handlePassword}></input>
+      <button id='login-button'>Sign In</button>
     </form>
   )};
 
