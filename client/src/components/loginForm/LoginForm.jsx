b5bef8e64ddfe5134f2b733c6e4b12bfa0d373a8
Sarah <sa.alashwal@gmail.com>
2021-07-14 21:55:54 -0400

    add login feature

--- client/src/components/loginForm/LoginForm.jsx
+++ client/src/components/loginForm/LoginForm.jsx
@@ -5,24 +5,28 @@ import '../LoginForm.css';
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [show, setShow] = useState(true);
 
   const handleUsername = (event) => setUsername(event.target.value);
   const handlePassword = (event) => setPassword(event.target.value);
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('/user', {
+    axios.post('http://localhost:5000/user', {
       username: username,
       password: password
     })
-    .then((response) => console.log(response))
+    .then((response) => response.data ? setShow(false) : setShow(true))
     .catch((error) => console.log(error))
   }
   return (
-    <form className='form-container' onSubmit={handleSubmit}>
+    <>
+    {show && 
+      <form className='form-container' onSubmit={handleSubmit}>
       <input placeholder='Username' type='text' className='form-input' onChange={handleUsername}></input>
       <input placeholder='Password' type='password' className='form-input' onChange={handlePassword}></input>
       <button className='form-button'>Sign In</button>
-    </form>
+    </form> }
+    </>
   )};
 
 export default LoginForm;
