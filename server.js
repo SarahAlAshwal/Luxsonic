62d71f43b5fba2af664bba6d897336a210f016e8
Sarah <sa.alashwal@gmail.com>
2021-07-15 20:53:20 -0400

    add get dashboard in server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = 5000;
 const cors = require('cors');
 const pool = require('./db');
 const { restart } = require('nodemon');
+const { response } = require('express');
 
 app.use(cors());
 
@@ -16,7 +17,7 @@ try {
   const { formInput } = req.body;
   const { newInput } = await pool.query(
   `INSERT INTO forms (firstName, lastName, form_uid, form_date, profission) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
-  [formInput.firstName, formInput.lastName, formInput.uid, formInput.date, formInput.profession]
+  [formInput.firstName, formInput.lastName, formInput.uid, formInput.date, formInput.profission]
   );
   res.status(200);
   }
@@ -30,8 +31,14 @@ app.post('/user', async(req, res) => {
     const { username, password } = req.body;
      pool.query(
       `SELECT * FROM users WHERE username = $1 AND password = $2;`, [username, password]
-    ).then (response => res.send(response.rows[0]) )
+    ).then (response => res.send(response.rows[0]))
       .catch(error => console.log(error));
+});
+
+app.get('/dashboard', async(req, res) => {
+  pool.query(`SELECT * FROM forms;`)
+  .then (response => res.send(response.rows))
+  .catch(error => console.log(error))
 });
 
 //middleware
